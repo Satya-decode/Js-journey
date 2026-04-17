dc7953f6becff29519ff7bd742a462f67e4dc32b
Marco <satyamyadav19sep@gmail.com>
2026-04-17 17:31:30 +0530

    Basics last

---
+++ 01_basics/07_dates_time.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+//let createdDate  = new Date(2025,9,19,11,30,25)
+let createdDate = new
+console.log(createdDate.toLocaleString());
+
+
