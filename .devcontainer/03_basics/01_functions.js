f2c8f23b79c89cb3f2adfe66f3bb727b235a83ec
Marco <satyamyadav19sep@gmail.com>
2026-05-04 12:32:35 +0530

    fresh

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,11 @@
+function sayMyName(){
+    console.log("M");
+    console.log("I");
+    console.log("C");
+    console.log("K");
+    console.log("E");
+    console.log("Y");
+    console.log("Z");
+}
+
+sayMyName()
